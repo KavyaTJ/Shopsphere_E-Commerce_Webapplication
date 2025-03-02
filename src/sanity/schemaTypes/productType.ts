c96fa02de25ee8ed454accb1ad54ec1728b036e0
KavyaTJ <kavyatj2609@gmail.com>
2025-03-02 18:06:13 +0530

    chore(utils): Add image URL processing and utility functions

--- src/sanity/schemaTypes/productType.ts
+++ src/sanity/schemaTypes/productType.ts
@@ -14,6 +14,14 @@ export const productType = defineType({
             validation:Rule=>Rule.required()
         }),
         defineField({
+            name:'slug',
+            title:'Slug',
+            type:'slug',
+            options:{
+                source:'name',
+                maxLength:96}
+        }),
+        defineField({
             name:'image',
             title:'Product Image',
             type:'image',
